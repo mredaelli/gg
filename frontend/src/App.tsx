f90647b097f42e1f933af55c0d112486f120fec0
Massimo Redaelli <massimo@typish.io>
2023-06-15 16:43:48 +0200

    access db

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,15 @@ import type { Component } from 'solid-js';
 
 import styles from './App.module.css';
 
+async function list() {
+  const endpoint = '/data-api/rest/Game';
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  console.table(data.value);
+}
+
+await list()
+
 const App: Component = () => {
   return (
     <div class={styles.App}>
